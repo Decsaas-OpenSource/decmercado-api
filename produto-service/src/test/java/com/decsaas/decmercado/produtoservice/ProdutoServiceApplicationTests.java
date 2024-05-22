185167cd6a47abda8d2ff2ec9f83c7422701d618
rodrigo <rodrigo_c91@hotmail.com>
2024-05-22 09:51:42 -0300

    inicio da arquitetura

---
+++ produto-service/src/test/java/com/decsaas/decmercado/produtoservice/ProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decsaas.decmercado.produtoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
